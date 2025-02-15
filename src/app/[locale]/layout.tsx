2a8aa9c3e89f10c807f0d6a8924569569b04940d
Alirea Hosseini <mehdimj0161@gmail.com>
2025-02-15 23:51:06 +0330

    refactor(RootLayout): - remove baselayout

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,23 +1,36 @@
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
-import BaseLayout from "@/providers/BaseLayout";
-import LocaleProvider from "@/providers/LocaleProvider";
 import { ILocales, IReactNode } from "../../../@types/Global";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import LocaleProvider from "@/providers/LocaleProvider";
 
 type Props = {
   children: IReactNode;
   params: { locale: ILocales };
 };
 
-export default async function LocaleLayout({ children, params }: Props) {
+export default async function RootLayout({ children, params }: Props) {
   const { locale } = await params;
-  if (!routing.locales.includes(locale as ILocales)) {
+  
+  if (!routing.locales.includes(locale)) {
     notFound();
   }
+  
+  const dir = locale === "fa" ? "rtl" : "ltr";
 
   return (
-    <BaseLayout locale={locale}>
-      <LocaleProvider locale={locale}>{children}</LocaleProvider>
-    </BaseLayout>
+    <html lang={locale} dir={dir} suppressHydrationWarning >
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          <LocaleProvider locale={locale}>
+            {children}
+          </LocaleProvider>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,11 @@
 import { routing } from "@/i18n/routing";
-import BaseLayout from "@/providers/BaseLayout";
 
 export default function GlobalNotFound() {
   return (
-    <BaseLayout locale={routing.defaultLocale}>
-      <p>Not found</p>
-    </BaseLayout>
+    <html>
+      <body>
+        <p>Not found</p>
+      </body>
+    </html>
   );
 }
