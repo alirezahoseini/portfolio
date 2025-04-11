244c09ee69cc27c81a6b1839d5394aa23b1de1ab
Alirea Hosseini <mehdimj0161@gmail.com>
2025-04-12 00:49:54 +0330

    feat(contact page): translate faqs

--- src/app/api/faqs/faqs.ts
+++ src/app/api/faqs/faqs.ts
@@ -2,34 +2,51 @@ export const faqs = {
   en: [
     {
       id: 1,
-      title: "What is your current role?",
-      desc: `I take all kinds of development and designing related projects. Which includes
-      UI/UX designing, frontend development, graphic designing.`
+      title: "How can I collaborate with you?",
+      desc: `It's easy! You can reach out to me through the contact form, 
+      or if you have a specific question, feel free to email me. Once I have
+      the necessary details, we can discuss the project and start working together.`
     },
     {
       id: 2,
-      title: "How much does it cost for a high performing website?",
-      desc: `It will depend upon the scope and requirements of the project. In a meeting, 
-      we will seek to identify specifics and objectives like number of pages, specific 
-      features, timeline, etc. All these will contribute to the cost and will vary from 
-      project to project.`
+      title: "What types of projects do you work on?",
+      desc: `I mainly focus on front-end projects, from designing and developing 
+      landing pages to building online stores and corporate websites. What's important
+      to me is ensuring that every project I work on is user-friendly, visually appealing, and functional.`
+    },
+    {
+      id: 3,
+      title: "Can you deliver complete front-end projects?",
+      desc: `Yes, I work with a great team, and together we can deliver front-end projects
+      from start to finish. From designing and developing web pages to implementing all the
+      necessary features and interactions, everything is done with care to provide a flawless user experience.`
     }
   ],
 
   fa: [
     {
       id: 1,
-      title: "نقش فعلی شما چیست؟",
-      desc: `من انواع پروژه‌های مرتبط با توسعه و طراحی را انجام می‌دهم. این شامل
-      طراحی رابط کاربری/تجربه کاربری، توسعه فرانت‌اند و طراحی گرافیک می‌شود.`
+      title: "چطور می‌توانم با شما همکاری کنم؟",
+      desc: `خیلی راحت! می‌تونید از طریق فرم تماس با
+      من ارتباط برقرار کنید، یا اگر سوال خاصی دارید، می‌تونید به 
+      ایمیلم پیام بدید. بعد از این که اطلاعات لازم رو دریافت کنم،
+      می‌تونیم در مورد جزئیات پروژه صحبت کنیم و همکاری رو شروع کنیم.`
     },
     {
       id: 2,
-      title: "هزینه یک وب‌سایت با عملکرد بالا چقدر است؟",
-      desc: `این به محدوده و نیازمندی‌های پروژه بستگی دارد. در جلسه، 
-      ما به دنبال شناسایی جزئیات و اهداف خاص مانند تعداد صفحات، 
-      ویژگی‌های خاص، زمان‌بندی و غیره خواهیم بود. تمام این موارد در هزینه تأثیرگذار 
-      هستند و از پروژه به پروژه متفاوت خواهند بود.`
+      title: "شما چه نوع پروژه‌هایی انجام می‌دهید؟",
+      desc: `من بیشتر روی پروژه‌های فرانت‌اند تمرکز دارم،
+      از طراحی و توسعه لندینگ پیج‌ها گرفته تا ساخت فروشگاه‌های 
+      آنلاین و وب‌سایت‌های شرکتی. برای من مهم اینه که هر پروژه‌ای که
+      انجام می‌دم، کاربرپسند، زیبا و عملکرد مناسبی داشته باشه.`
+    },
+    {
+      id: 3,
+      title: "آیا می‌توانید پروژه‌های فرانت‌اند را به طور کامل تحویل دهید؟",
+      desc: `بله، من با یک تیم عالی همکاری
+      می‌کنم که می‌تونیم پروژه‌های فرانت‌اند رو از ابتدا تا انتها تحویل بدیم. 
+      از طراحی و توسعه صفحات وب تا پیاده‌سازی تمامی ویژگی‌ها
+      و تعاملات مورد نیاز، همه چیز به دقت انجام می‌شه تا تجربه کاربری بی‌نقصی ارائه بشه.`
     }
   ]
 }
