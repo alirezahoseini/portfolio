9b3f87d6788e203a745ec30bc23bbffec7704034
Alirea Hosseini <mehdimj0161@gmail.com>
2025-04-14 01:02:00 +0330

    feat(projects): update project details for Next Movie and E-Shop

--- src/app/api/projects/projects.ts
+++ src/app/api/projects/projects.ts
@@ -1,109 +1,39 @@
 export const projects = {
   en: [
     {
-      id: "banimode",
-      title: "Banimode Landing",
-      date: 2021,
-      backgroundColor: "#99ffb4",
-      description: `In my first experience with React, I made a completely static version 
-      of the Banimode landing page. The main goal of this project was to practice 
-      basic concepts like structuring components, using props, and organizing code
-      better. I tried to rebuild the UI using components to understand how React works.`,
+      id: "nextmovie",
+      title: "Next Movie",
+      date: 2024,
+      backgroundColor: "#ff1a1a",
+      description: `In this app, you can explore the latest movies and TV series and get complete
+      information about them. There's also a search feature to find your favorite titles. This project
+      is built using Next.js version 14 and Tailwind CSS, and it fetches data from the TMDB API. It 
+      also uses SWR for optimized data fetching and caching. I gained valuable experience with SSR and
+      SSG during this project, especially in managing content rendering and search functionality.`,
       coverImage: {
-        url: "/projects/banimode/cover.png",
-        alt: "banimode cover"
+        url: "/projects/nextmovie/cover.png",
+        alt: "cover nextmovie"
       },
       heroImage: {
-        url: "/projects/banimode/hero.png",
-        alt: "banimode hero"
+        url: "/projects/nextmovie/hero.png",
+        alt: "hero nextmovie"
       },
       technologies: [
-        "Bootstrap",
+        "Next.js",
+        "Tailwind",
+        "JavaScript",
         "React",
-        "JavaScript",
-        "Sass"
+        "SWR",
+        "TMDB"
       ],
       screenshots: [
-        "/projects/banimode/1.png"
+        "/projects/nextmovie/1.png",
+        "/projects/nextmovie/2.png",
+        "/projects/nextmovie/3.png"
       ],
       links: {
-        github: "https://github.com/alirezahoseini/Banimode-Landing-Page",
-        live: "https://banimode.a-hosseini.ir/"
-      }
-    },
-    {
-      id: "todojs",
-      title: "TODO",
-      date: 2021,
-      backgroundColor: "#b0daff",
-      description: `
-      This is one of the most complete todo apps I’ve built using plain JavaScript.
-      Main features include:
-      Task search,
-      Task categorization,
-      Priority levels,
-      Data saved with localStorage,
-      Sound effects for better user interaction.
-      I built this project as a serious practice to improve my JavaScript skills, 
-      DOM manipulation, and front-end architecture.
-      `,
-      coverImage: {
-        url: "/projects/todojs/cover.png",
-        alt: "todo app cover"
-      },
-      heroImage: {
-        url: "/projects/todojs/hero.png",
-        alt: "todo app hero"
-      },
-      technologies: [
-        "Bootstrap",
-        "JavaScript",
-        "Sass"
-      ],
-      screenshots: [
-        "/projects/todojs/1.png",
-        "/projects/todojs/2.png",
-        "/projects/todojs/3.png"
-      ],
-      links: {
-        github: "https://github.com/alirezahoseini/Todo-App-Js",
-        live: "https://alirezahoseini.github.io/Todo-App-Js/"
-      }
-    },
-    {
-      id: "eshop",
-      title: "E-Shop",
-      date: 2021,
-      backgroundColor: "#ffe5b6",
-      description: `eShop is a static store template that shows product data from local JSON files.
-      It was my first big JavaScript practice project, and even though I didn’t have much experience
-      at the time, I tried to make everything clean, user-friendly, and as professional as possible.
-      Some of its features include a custom slideshow, shopping cart and wishlist, notifications, form
-      validation using Regex, sticky header, fully responsive design, and Swiper slider.`,
-      coverImage: {
-        url: "/projects/eshop/cover.png",
-        alt: "cover eshop"
-      },
-      heroImage: {
-        url: "/projects/eshop/hero.png",
-        alt: "hero eshop"
-      },
-      technologies: [
-        "Bootstrap",
-        "JavaScript",
-        "Regex",
-        "Swiper Slider"
-      ],
-      screenshots: [
-        "/projects/eshop/1.png",
-        "/projects/eshop/2.png",
-        "/projects/eshop/3.png",
-        "/projects/eshop/1.gif",
-        "/projects/eshop/2.gif"
-      ],
-      links: {
-        github: "https://github.com/alirezahoseini/E-shop",
-        live: "https://alirezahoseini.github.io/E-shop/pages/home.html"
+        github: "https://github.com/alirezahoseini/next-movie",
+        live: "https://nextmovie.a-hosseini.ir/"
       }
     },
     {
@@ -177,135 +107,22 @@ export const projects = {
       }
     },
     {
-      id: "nextmovie",
-      title: "Next Movie",
-      date: 2024,
-      backgroundColor: "#ff1a1a",
-      description: `In this app, you can explore the latest movies and TV series and get complete
-      information about them. There's also a search feature to find your favorite titles. This project
-      is built using Next.js version 14 and Tailwind CSS, and it fetches data from the TMDB API. It 
-      also uses SWR for optimized data fetching and caching. I gained valuable experience with SSR and
-      SSG during this project, especially in managing content rendering and search functionality.`,
-      coverImage: {
-        url: "/projects/nextmovie/cover.png",
-        alt: "cover nextmovie"
-      },
-      heroImage: {
-        url: "/projects/nextmovie/hero.png",
-        alt: "hero nextmovie"
-      },
-      technologies: [
-        "Next.js",
-        "Tailwind",
-        "JavaScript",
-        "React",
-        "SWR",
-        "TMDB"
-      ],
-      screenshots: [
-        "/projects/nextmovie/1.png",
-        "/projects/nextmovie/2.png",
-        "/projects/nextmovie/3.png"
-      ],
-      links: {
-        github: "https://github.com/alirezahoseini/next-movie",
-        live: "https://nextmovie.a-hosseini.ir/"
-      }
-    }
-  ],
-
-  fa: [
-    {
-      id: "banimode",
-      title: "Banimode Landing",
-      date: 2021,
-      backgroundColor: "#99ffb4",
-      description: `در اولین تجربه‌ام با React، یک نسخه‌ی کاملاً استاتیک از لندینگ پیج
-       بانی‌مد پیاده‌سازی کردم. هدف اصلی این پروژه،
-       تمرین مفاهیم پایه‌ای مانند ساختاردهی کامپوننت‌ها، مدیریت props و سازماندهی بهتر کد بود. در این پروژه
-       تلاش کردم 
-      رابط کاربری را به صورت component-based بازسازی کنم تا بهتر با طرز فکر React آشنا شوم.`,
-      coverImage: {
-        url: "/projects/banimode/cover.png",
-        alt: "تصویر کاور پروژه بانی مد"
-      },
-      heroImage: {
-        url: "/projects/banimode/hero.png",
-        alt: "تصویر هدر پروژه بانی مد"
-      },
-      technologies: [
-        "Bootstrap",
-        "React",
-        "JavaScript",
-        "Sass"
-      ],
-      screenshots: [
-        "/projects/banimode/1.png"
-      ],
-      links: {
-        github: "https://github.com/alirezahoseini/Banimode-Landing-Page",
-        live: "https://banimode.a-hosseini.ir/"
-      }
-    },
-    {
-      id: "todojs",
-      title: "TODO",
-      date: 2021,
-      backgroundColor: "#b0daff",
-      description: `
-      این برنامه یکی از کامل‌ترین Todo Appهایی هست که با جاوااسکریپت Vanilla طراحی کردم.
-      ویژگی‌های اصلی برنامه شامل موارد زیرهست:
-      جستجوی کارها،
-      دسته‌بندی وظایف،
-      اولویت‌ بندی کارها،
-      ذخیره‌سازی داده‌ها با استفاده از localStorage،
-      استفاده از افکت‌های صوتی برای تعامل بهتر با کاربر.
-      این پروژه رو به عنوان یک تمرین جدی برای تسلط بیشتر روی جاوااسکریپت، مدیریت DOM،
-      و معماری سمت فرانت پیاده‌سازی کردم.
-      `,
-      coverImage: {
-        url: "/projects/todojs/cover.png",
-        alt: "تصویر کاور پروژه تودو"
-      },
-      heroImage: {
-        url: "/projects/todojs/hero.png",
-        alt: "تصویر هدر پروژه تودو"
-      },
-      technologies: [
-        "Bootstrap",
-        "JavaScript",
-        "Sass"
-      ],
-      screenshots: [
-        "/projects/todojs/1.png",
-        "/projects/todojs/2.png",
-        "/projects/todojs/3.png"
-      ],
-      links: {
-        github: "https://github.com/alirezahoseini/Todo-App-Js",
-        live: "https://alirezahoseini.github.io/Todo-App-Js/"
-      }
-    },
-    {
       id: "eshop",
       title: "E-Shop",
       date: 2021,
       backgroundColor: "#ffe5b6",
-      description: `eShop یک قالب فروشگاهی استاتیکه که
-       اطلاعات محصولات رو به صورت داینامیک از فایل‌های JSON محلی دریافت می‌کنه. 
-      این پروژه اولین تمرین بزرگ من با جاوااسکریپت بود و با وجود اینکه هنوز تجربه زیادی نداشتم،
-      تلاش کردم همه چیز رو تمیز، کاربرپسند و تا حد امکان حرفه‌ای پیاده‌سازی کنم.
-      از امکانات این قالب می‌تونم به
-      اسلایدشو سفارشی،
-      سبد خرید و لیست علاقه‌مندی، نوتیفیکیشن، اعتبارسنجی فرم با Regex، هدر چسبنده، طراحی کاملاً ریسپانسیو و استفاده
-      از Swiper Slider اشاره کنم.`,
+      description: `eShop is a static store template that shows product data from local JSON files.
+      It was my first big JavaScript practice project, and even though I didn’t have much experience
+      at the time, I tried to make everything clean, user-friendly, and as professional as possible.
+      Some of its features include a custom slideshow, shopping cart and wishlist, notifications, form
+      validation using Regex, sticky header, fully responsive design, and Swiper slider.`,
       coverImage: {
         url: "/projects/eshop/cover.png",
-        alt: "تصویر کاور پروژه eshop"
+        alt: "cover eshop"
       },
       heroImage: {
         url: "/projects/eshop/hero.png",
-        alt: "تصویر هدر پروژه eshop"
+        alt: "hero eshop"
       },
       technologies: [
         "Bootstrap",
@@ -323,6 +140,116 @@ export const projects = {
       links: {
         github: "https://github.com/alirezahoseini/E-shop",
         live: "https://alirezahoseini.github.io/E-shop/pages/home.html"
+      }
+    },
+    {
+      id: "todojs",
+      title: "TODO",
+      date: 2021,
+      backgroundColor: "#b0daff",
+      description: `
+      This is one of the most complete todo apps I’ve built using plain JavaScript.
+      Main features include:
+      Task search,
+      Task categorization,
+      Priority levels,
+      Data saved with localStorage,
+      Sound effects for better user interaction.
+      I built this project as a serious practice to improve my JavaScript skills, 
+      DOM manipulation, and front-end architecture.
+      `,
+      coverImage: {
+        url: "/projects/todojs/cover.png",
+        alt: "todo app cover"
+      },
+      heroImage: {
+        url: "/projects/todojs/hero.png",
+        alt: "todo app hero"
+      },
+      technologies: [
+        "Bootstrap",
+        "JavaScript",
+        "Sass"
+      ],
+      screenshots: [
+        "/projects/todojs/1.png",
+        "/projects/todojs/2.png",
+        "/projects/todojs/3.png"
+      ],
+      links: {
+        github: "https://github.com/alirezahoseini/Todo-App-Js",
+        live: "https://alirezahoseini.github.io/Todo-App-Js/"
+      }
+    },
+    {
+      id: "banimode",
+      title: "Banimode Landing",
+      date: 2021,
+      backgroundColor: "#99ffb4",
+      description: `In my first experience with React, I made a completely static version 
+      of the Banimode landing page. The main goal of this project was to practice 
+      basic concepts like structuring components, using props, and organizing code
+      better. I tried to rebuild the UI using components to understand how React works.`,
+      coverImage: {
+        url: "/projects/banimode/cover.png",
+        alt: "banimode cover"
+      },
+      heroImage: {
+        url: "/projects/banimode/hero.png",
+        alt: "banimode hero"
+      },
+      technologies: [
+        "Bootstrap",
+        "React",
+        "JavaScript",
+        "Sass"
+      ],
+      screenshots: [
+        "/projects/banimode/1.png"
+      ],
+      links: {
+        github: "https://github.com/alirezahoseini/Banimode-Landing-Page",
+        live: "https://banimode.a-hosseini.ir/"
+      }
+    }
+  ],
+
+  fa: [
+    {
+      id: "nextmovie",
+      title: "Next Movie",
+      date: 2024,
+      backgroundColor: "#ff1a1a",
+      description: `در این اپلیکیشن می‌تونی جدیدترین فیلم‌ها
+      و سریال‌های روز رو ببینی و اطلاعات کاملی درباره‌شون داشته باشی.
+      امکان جستجو هم برای پیدا کردن فیلم یا سریال مورد علاقه‌ات وجود داره. این پروژه با استفاده از 
+      Next.js نسخه ۱۴ و Tailwind CSS ساخته شده و دیتا از طریق API سایت TMDB دریافت می‌شه.
+      همچنین با استفاده از SWR دیتا به‌صورت بهینه دریافت و کش می‌شه.
+      تجربه‌ی خوبی از کار با SSR و SSG توی این پروژه داشتم، مخصوصاً در مدیریت رندر شدن محتوا و جستجو.`,
+      coverImage: {
+        url: "/projects/nextmovie/cover.png",
+        alt: "تصویر کاور پروژه nextmovie"
+      },
+      heroImage: {
+        url: "/projects/nextmovie/hero.png",
+        alt: "تصویر هدر پروژه nextmovie"
+      },
+      technologies: [
+        "Next.js",
+        "Tailwind",
+        "JavaScript",
+        "React",
+        "SWR",
+        "TMDB"
+      ],
+      screenshots: [
+        "/projects/nextmovie/1.png",
+        "/projects/nextmovie/2.png",
+        "/projects/nextmovie/3.png"
+      ],
+      links: {
+        github: "https://github.com/alirezahoseini/next-movie",
+        live: "https://nextmovie.a-hosseini.ir/"
       }
     },
     {
@@ -397,40 +324,113 @@ export const projects = {
       }
     },
     {
-      id: "nextmovie",
-      title: "Next Movie",
-      date: 2024,
-      backgroundColor: "#ff1a1a",
-      description: `در این اپلیکیشن می‌تونی جدیدترین فیلم‌ها
-      و سریال‌های روز رو ببینی و اطلاعات کاملی درباره‌شون داشته باشی.
-      امکان جستجو هم برای پیدا کردن فیلم یا سریال مورد علاقه‌ات وجود داره. این پروژه با استفاده از 
-      Next.js نسخه ۱۴ و Tailwind CSS ساخته شده و دیتا از طریق API سایت TMDB دریافت می‌شه.
-      همچنین با استفاده از SWR دیتا به‌صورت بهینه دریافت و کش می‌شه.
-      تجربه‌ی خوبی از کار با SSR و SSG توی این پروژه داشتم، مخصوصاً در مدیریت رندر شدن محتوا و جستجو.`,
+      id: "eshop",
+      title: "E-Shop",
+      date: 2021,
+      backgroundColor: "#ffe5b6",
+      description: `eShop یک قالب فروشگاهی استاتیکه که
+       اطلاعات محصولات رو به صورت داینامیک از فایل‌های JSON محلی دریافت می‌کنه. 
+      این پروژه اولین تمرین بزرگ من با جاوااسکریپت بود و با وجود اینکه هنوز تجربه زیادی نداشتم،
+      تلاش کردم همه چیز رو تمیز، کاربرپسند و تا حد امکان حرفه‌ای پیاده‌سازی کنم.
+      از امکانات این قالب می‌تونم به
+      اسلایدشو سفارشی،
+      سبد خرید و لیست علاقه‌مندی، نوتیفیکیشن، اعتبارسنجی فرم با Regex، هدر چسبنده، طراحی کاملاً ریسپانسیو و استفاده
+      از Swiper Slider اشاره کنم.`,
       coverImage: {
-        url: "/projects/nextmovie/cover.png",
-        alt: "تصویر کاور پروژه nextmovie"
+        url: "/projects/eshop/cover.png",
+        alt: "تصویر کاور پروژه eshop"
       },
       heroImage: {
-        url: "/projects/nextmovie/hero.png",
-        alt: "تصویر هدر پروژه nextmovie"
+        url: "/projects/eshop/hero.png",
+        alt: "تصویر هدر پروژه eshop"
       },
       technologies: [
-        "Next.js",
-        "Tailwind",
+        "Bootstrap",
         "JavaScript",
-        "React",
-        "SWR",
-        "TMDB"
+        "Regex",
+        "Swiper Slider"
       ],
       screenshots: [
-        "/projects/nextmovie/1.png",
-        "/projects/nextmovie/2.png",
-        "/projects/nextmovie/3.png"
+        "/projects/eshop/1.png",
+        "/projects/eshop/2.png",
+        "/projects/eshop/3.png",
+        "/projects/eshop/1.gif",
+        "/projects/eshop/2.gif"
       ],
       links: {
-        github: "https://github.com/alirezahoseini/next-movie",
-        live: "https://nextmovie.a-hosseini.ir/"
+        github: "https://github.com/alirezahoseini/E-shop",
+        live: "https://alirezahoseini.github.io/E-shop/pages/home.html"
+      }
+    },
+    {
+      id: "todojs",
+      title: "TODO",
+      date: 2021,
+      backgroundColor: "#b0daff",
+      description: `
+      این برنامه یکی از کامل‌ترین Todo Appهایی هست که با جاوااسکریپت Vanilla طراحی کردم.
+      ویژگی‌های اصلی برنامه شامل موارد زیرهست:
+      جستجوی کارها،
+      دسته‌بندی وظایف،
+      اولویت‌ بندی کارها،
+      ذخیره‌سازی داده‌ها با استفاده از localStorage،
+      استفاده از افکت‌های صوتی برای تعامل بهتر با کاربر.
+      این پروژه رو به عنوان یک تمرین جدی برای تسلط بیشتر روی جاوااسکریپت، مدیریت DOM،
+      و معماری سمت فرانت پیاده‌سازی کردم.
+      `,
+      coverImage: {
+        url: "/projects/todojs/cover.png",
+        alt: "تصویر کاور پروژه تودو"
+      },
+      heroImage: {
+        url: "/projects/todojs/hero.png",
+        alt: "تصویر هدر پروژه تودو"
+      },
+      technologies: [
+        "Bootstrap",
+        "JavaScript",
+        "Sass"
+      ],
+      screenshots: [
+        "/projects/todojs/1.png",
+        "/projects/todojs/2.png",
+        "/projects/todojs/3.png"
+      ],
+      links: {
+        github: "https://github.com/alirezahoseini/Todo-App-Js",
+        live: "https://alirezahoseini.github.io/Todo-App-Js/"
+      }
+    },
+    {
+      id: "banimode",
+      title: "Banimode Landing",
+      date: 2021,
+      backgroundColor: "#99ffb4",
+      description: `در اولین تجربه‌ام با React، یک نسخه‌ی کاملاً استاتیک از لندینگ پیج
+       بانی‌مد پیاده‌سازی کردم. هدف اصلی این پروژه،
+       تمرین مفاهیم پایه‌ای مانند ساختاردهی کامپوننت‌ها، مدیریت props و سازماندهی بهتر کد بود. در این پروژه
+       تلاش کردم 
+      رابط کاربری را به صورت component-based بازسازی کنم تا بهتر با طرز فکر React آشنا شوم.`,
+      coverImage: {
+        url: "/projects/banimode/cover.png",
+        alt: "تصویر کاور پروژه بانی مد"
+      },
+      heroImage: {
+        url: "/projects/banimode/hero.png",
+        alt: "تصویر هدر پروژه بانی مد"
+      },
+      technologies: [
+        "Bootstrap",
+        "React",
+        "JavaScript",
+        "Sass"
+      ],
+      screenshots: [
+        "/projects/banimode/1.png"
+      ],
+      links: {
+        github: "https://github.com/alirezahoseini/Banimode-Landing-Page",
+        live: "https://banimode.a-hosseini.ir/"
       }
     }
   ]
